fb54bda31c96d68ed23abc845f94bda13568a5ab
Linkka <samilindroos@hotmail.com>
2016-02-01 20:53:10 +0200

    Initial commit.

---
+++ Kotitehtava2olio27012016/CD.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava2olio27012016
+{
+    class CD
+    {
+        // properties
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public int Year { get; set; }
+        public string Song { get; set; }
+        public string Song2 { get; set; }
+        public string Song3 { get; set; }
+        public string Song4 { get; set; }
+        public string Song5 { get; set; }
+        public string Song6 { get; set; }
+        public string Song7 { get; set; }
+        public string Song8 { get; set; }
+        public string Song9 { get; set; }
+        public string Song10 { get; set; }
+        public string Song11 { get; set; }
+        public string Song12 { get; set; }
+
+        // don't create any constructor, so default one will be used
+
+        public void PrintData()
+        {
+            Console.WriteLine("CD data : ");
+            Console.WriteLine("- Album name : " + Name);
+            Console.WriteLine("- Year : " + Year);
+            Console.WriteLine("- Artist : " + Artist);
+            Console.WriteLine("- Songs : " + Song);
+            Console.WriteLine("---- " + Song2);
+            Console.WriteLine("---- " + Song3);
+            Console.WriteLine("---- " + Song4);
+            Console.WriteLine("---- " + Song5);
+            Console.WriteLine("---- " + Song6);
+            Console.WriteLine("---- " + Song7);
+            Console.WriteLine("---- " + Song8);
+            Console.WriteLine("---- " + Song9);
+            Console.WriteLine("---- " + Song10);
+            Console.WriteLine("---- " + Song11);
+            Console.WriteLine("---- " + Song12);
+
+        }
+
+        // destructor
+        ~CD()
+        {
+            Console.WriteLine("CD object destroyed.");
+        }
+
+    }
+}
---
+++ Kotitehtava2olio27012016/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava2olio27012016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create CDs
+            CD cd1 = new CD();
+            cd1.Name = "Black Album";
+            cd1.Artist = "Metallica";
+            cd1.Year = 1991;
+            cd1.Song = "Enter Sandman";
+            cd1.Song2 = "Sad But True";
+            cd1.Song3 = "Holier Than Thou";
+            cd1.Song4 = "The Unforgiven";
+            cd1.Song5 = "Wherever I May Roam";
+            cd1.Song6 = "Don't Tread on Me";
+            cd1.Song7 = "Through the Never";
+            cd1.Song8 = "Nothing Else Matters";
+            cd1.Song9 = "Of Wolf and Man";
+            cd1.Song10 = "The God that Failed";
+            cd1.Song11 = "My Friend of Misery";
+            cd1.Song12 = "The Struggle Within";
+
+            cd1.PrintData();
+            Console.ReadLine();
+        }
+    }
+
+}
+
